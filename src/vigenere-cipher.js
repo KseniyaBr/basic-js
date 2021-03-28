07ff15acdad18d5701212d6d81960b76abcbb682
Kseniya Barkova <kcancer17@gmail.com>
2021-03-28 16:59:56 +0300

    feat: add the solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(modification) {
+    this.modification = modification ;
+  }
+
+  encrypt(message, key) {
+    if (message == null || key == null) throw Error();
+    const LATIN_LETTERS = /[A-Za-z]/;
+    const ALPHABET_LENGTH = 26;
+    const FIRST_LETTER_CODE = 'A'.charCodeAt();
+    let codingLetterCounter = 0;
+
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase();
+
+    message.forEach((element, index) => {
+      if (LATIN_LETTERS.test(element)) {
+        message[index] = String.fromCharCode(
+          ((element.charCodeAt() - FIRST_LETTER_CODE
+            + (key[codingLetterCounter % key.length].charCodeAt() - FIRST_LETTER_CODE))
+            % ALPHABET_LENGTH)
+          + FIRST_LETTER_CODE);
+        codingLetterCounter++;
+      }
+    });
+    return this.modification === false ? message.reverse().join('') : message.join('');
+  }
+  decrypt(message, key) {
+    if (message == null || key == null) throw Error();
+    const LATIN_LETTERS = /[A-Za-z]/;
+    const ALPHABET_LENGTH = 26;
+    const FIRST_LETTER_CODE = 'A'.charCodeAt();
+    let codingLetterCounter = 0;
+
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase();
+
+    message.forEach((element, index) => {
+      if (LATIN_LETTERS.test(element)) {
+        message[index] = String.fromCharCode(
+          ((element.charCodeAt() - FIRST_LETTER_CODE
+            + (ALPHABET_LENGTH - (key[codingLetterCounter % key.length].charCodeAt() - FIRST_LETTER_CODE)))
+            % ALPHABET_LENGTH)
+          + FIRST_LETTER_CODE);
+        codingLetterCounter++;
+      }
+    });
+    return this.modification === false ? message.reverse().join('') : message.join('');
   }
 }
 
