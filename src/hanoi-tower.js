c69073d2bdfa6e1f696145bab4364f35a9f76e6c
Kseniya Barkova <kcancer17@gmail.com>
2021-03-29 16:12:57 +0300

    fix: fix the solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,9 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  let result = {
-		turns: (2 ** disksNumber) - 1,
-	};
-	result['seconds'] = Math.floor((3600 / turnsSpeed) * result.turns);
-	return result;
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let turnsCount;
+  let secondsCount;
+  // Perform calculations (rounded down)
+  turnsCount = Math.floor(2 ** [disksNumber] - 1);
+  secondsCount = Math.floor(turnsCount / turnsSpeed * 3600);
+  // Return an object with appropriate properties
+  return {
+    turns: turnsCount,
+    seconds: secondsCount
+  }
 };
